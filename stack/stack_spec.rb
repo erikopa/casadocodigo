22e8e26e7f167911d67fc2f6dd812f05311d72d0
Eriko Morais <erikopa@gmail.com>
2015-09-20 17:00:13 -0300

    Exemplos livros Casa do Código

---
+++ stack/stack_spec.rb
@@ -0,0 +1,45 @@
+class Stack
+	
+	def initialize
+		@elements = []
+	end
+
+	def push(element)
+		@elements << element
+	end
+
+	def top
+		@elements[-1]
+	end
+
+	def pop
+		@elements.delete_at(-1)
+	end
+
+end
+
+describe Stack do 
+	describe "#push" do
+		it "puts an element at the top of the stack" do
+			stack = Stack.new
+
+			stack.push(1)
+			stack.push(2)
+
+			expect(stack.top).to eq(2)
+		end
+	end
+
+	describe "#pop"	do
+		it "pop an element at the top of the stack" do
+			stack = Stack.new
+
+			stack.push(1)
+			stack.push(2)
+			stack.pop()
+			
+			expect(stack.top).to eq(1)
+
+		end
+	end
+end
